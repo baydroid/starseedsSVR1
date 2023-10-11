4dfe1c607396843dec8042661747552152af4911
baydroid <29621214+baydroid@users.noreply.github.com>
2023-10-11 01:49:39 -0700

    Split into A, B & C widgets

--- express.js
+++ express.js
@@ -1,33 +1,79 @@
-const fs      = require('fs');
 const express = require('express');
 
 
 
-const APP                   = express();
-const PORT_NUMBER           = 3000;
-const SOURCE_DIR            = 'dist';
+const HELLO_WORLD_HTML =
+`<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Hello World</title>
+</head>
+<body>
 
-const API_KEY               = process.env.DODOEX_WIDGET_API_KEY ? process.env.DODOEX_WIDGET_API_KEY : "";
-const FEE_RATE              = process.env.DODOEX_WIDGET_FEE_RATE ? Number.parseInt(process.env.DODOEX_WIDGET_FEE_RATE) : 0;
-const REBATE_ADDRESS        = process.env.DODOEX_WIDGET_REBATE_ADDRESS ? process.env.DODOEX_WIDGET_REBATE_ADDRESS : "";
+<h1>Hello World</h1>
 
-const SETTINGS_FILE         = "dist/swapwidget/settings.js";
-const SETTINGS_FILE_CONTENT = `window.starseedsSwapWidget = { API_KEY: "${ API_KEY }", FEE_RATE: ${ FEE_RATE }, REBATE_ADDRESS: "${ REBATE_ADDRESS }" };\n`;
+</body>
+</html>
+`;
 
-try
+function helloWorld(req, res)
     {
-    fs.writeFileSync(SETTINGS_FILE, SETTINGS_FILE_CONTENT);
+    res.send(HELLO_WORLD_HTML);
     }
-catch (err)
+
+
+
+const SUFFIXES = [ "A", "B", "C" ];
+
+let paramsJS = "";
+
+function sendParamsJS(req, res)
     {
-    console.error(err);
+    res.send(generateParamsJS());
     }
-APP.use(express.static(SOURCE_DIR));
+
+function generateParamsJS()
+    {
+    if (!paramsJS) paramsJS = "window.starseedsSwapWidget = " + loadAllParams() + ";\n";
+    return paramsJS;
+    }
+
+function loadAllParams()
+    {
+    let params = "{ ";
+    for (let sfx of SUFFIXES) params += `${ sfx }: ${ load1ParamSet(sfx) }, `;
+    return params + "}";
+    }
+
+function load1ParamSet(suffix)
+    {
+    let params = "{ ";
+    const apiKey = process.env["WIDGET_API_KEY_" + suffix];
+    params += `API_KEY: "${ apiKey ? apiKey : "" }", `;
+    const rebateAddress = process.env["WIDGET_REBATE_ADDRESS_" + suffix];
+    params += `REBATE_ADDRESS: "${ rebateAddress ? rebateAddress : "" }", `;
+    const feeRate = process.env["WIDGET_FEE_RATE_" + suffix];
+    params += `FEE_RATE: ${ feeRate ? Number.parseInt(feeRate) : 0 }, `;
+    return params + "}";
+    }
+
+
+
+const APP         = express();
+const PORT_NUMBER = 3000;
+
+for (let sfx of SUFFIXES)
+    {
+    APP.use(`/swapwidget/${ sfx               }`, express.static('dist/swapwidget'));
+    APP.use(`/swapwidget/${ sfx.toLowerCase() }`, express.static('dist/swapwidget'));
+    }
+APP.all('/params', sendParamsJS);
+APP.all('/', helloWorld);
+APP.all('/index.html', helloWorld);
 APP.listen(PORT_NUMBER, () =>
     {
-    console.log(`Express web server started   http://localhost:${ PORT_NUMBER }`);
-    console.log(`Serving content from         /${ SOURCE_DIR }`);
-    console.log(`DODOEX_WIDGET_API_KEY        ${ API_KEY }`);
-    console.log(`DODOEX_WIDGET_FEE_RATE       ${ FEE_RATE }`);
-    console.log(`DODOEX_WIDGET_REBATE_ADDRESS ${ REBATE_ADDRESS }`);
+    console.log(`Starseeds web server started http://localhost:${ PORT_NUMBER }`);
+    console.log(`${ generateParamsJS() }`);
     });
